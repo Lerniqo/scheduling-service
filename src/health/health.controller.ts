909d53e20eaa4777429e08fe61b461929da68d8d
Devinda Dilshan <devindadilshan0@gmail.com>
2025-10-16 23:02:32 +0530

    change health endpoint

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,33 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import {
-  HealthCheck,
-  HealthCheckService,
-  TypeOrmHealthIndicator,
-  MemoryHealthIndicator,
 } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
-    private readonly health: HealthCheckService,
-    private readonly db: TypeOrmHealthIndicator,
-    private readonly memory: MemoryHealthIndicator,
   ) {}
 
   @Get('live')
-  @HealthCheck()
   liveness() {
-    return this.health.check([
-      // Check that the process has at least some memory headroom (e.g., < 300MB heap used)
-      () => this.memory.checkHeap('memory_heap', 300 * 1024 * 1024),
-    ]);
-  }
-
-  @Get('ready')
-  @HealthCheck()
-  readiness() {
-    return this.health.check([
-      () => this.db.pingCheck('database', { timeout: 5000 }),
-    ]);
+    return {
+      "status": "ok",
+      "message": "Liveness probe successful"
+    };
   }
 }
